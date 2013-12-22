b065bc814c74f4e968baf9c0094f23556dd1dad8
Special Leung <specialcyci@gmail.com>
2013-12-22 12:47:02 +0800

    create demo project.

---
+++ ResideMenuDemo/src/com/special/ResideMenuDemo/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.special.ResideMenuDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
